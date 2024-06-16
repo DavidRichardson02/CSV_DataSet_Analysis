6ad0e71b798525795854c8a6cbc55e0247bac7e7
David Richardson <02richardsondavid@gmail.com>
2024-06-16 19:04:31 -0400

    Initial Commit

---
+++ CSV_File_Data_Set_Analysis/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CSV_File_Data_Set_Analysis
+//
+//  Created by David Richardson on 6/16/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
